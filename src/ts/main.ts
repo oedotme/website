4dfb255c18ae4c99d6c5a67cecb9d498cf30e429
iixixv <iixixv.me@gmail.com>
2018-10-27 11:07:44 +0200

    add scroll module

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,2 @@
 import './modules/nav'
+import './modules/scroll'
---
+++ src/ts/modules/scroll.ts
@@ -0,0 +1,7 @@
+// const aboutPage = document.querySelector('#about')
+
+// console.log(aboutPage.scrollTop)
+
+// document.addEventListener('scroll', () => {
+// 	console.log(aboutPage)
+// })
