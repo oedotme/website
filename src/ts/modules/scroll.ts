437e231a5a2f220556c89ff6e91d04a1eb0ccda5
iixixv <iixixv.me@gmail.com>
2019-10-22 14:04:56 +0200

    update scroll module

--- src/ts/modules/scroll.ts
+++ src/ts/modules/scroll.ts
@@ -5,6 +5,10 @@ const skills: HTMLElement = document.querySelector('#skills')
 const projects: HTMLElement = document.querySelector('#projects')
 const reach: HTMLElement = document.querySelector('#reach')
 
+let skills_link = document.querySelector('.skills-link')
+let projects_link = document.querySelector('.projects-link')
+let reach_link = document.querySelector('.reach-link')
+
 window.addEventListener('scroll', () => {
 	let top = window.scrollY
 	let home_height = home.offsetHeight
@@ -14,25 +18,18 @@ window.addEventListener('scroll', () => {
 
 	if (top + 200 < home_height) {
 		deactivateLink()
-
 		//
 	} else if (top > home_height && top < skills_height) {
 		deactivateLink()
-
-		let skillsLink = document.querySelector('.skills-link')
-		skillsLink.classList.add('active')
+		skills_link.classList.add('active')
 		//
 	} else if (top > skills_height && top < projects_height) {
 		deactivateLink()
-
-		let projectsLink = document.querySelector('.projects-link')
-		projectsLink.classList.add('active')
+		projects_link.classList.add('active')
 		//
 	} else if (top > projects_height && top < reach_height) {
 		deactivateLink()
-
-		let reachLink = document.querySelector('.reach-link')
-		reachLink.classList.add('active')
+		reach_link.classList.add('active')
 		//
 	}
 })
