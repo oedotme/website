b28cefdd9cc1caabaedd0400124d74cbcb4f1a23
oedotme <oedotme@gmail.com>
2021-05-09 14:45:24 +0200

    feat: remove base path from vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   assetsInclude: ['pdf'],
-  base: process.env.NODE_ENV === 'production' ? '/website/' : '/',
   resolve: { alias: { '@': '/src' } },
 })
