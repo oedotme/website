beefa0b6da7739bf53d0ebbc56d1a2501d38084c
oedotme <oedotme@gmail.com>
2021-05-09 14:36:05 +0200

    feat: move resume to src assets

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  assetsInclude: ['pdf'],
   base: process.env.NODE_ENV === 'production' ? '/website/' : '/',
   resolve: { alias: { '@': '/src' } },
 })
