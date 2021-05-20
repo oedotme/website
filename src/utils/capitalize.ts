39d119ec3c3393b89c98a950900f5e9bb863ddbe
oedotme <oedotme@gmail.com>
2021-05-20 11:36:52 +0200

    feat: update capitalize utility

--- src/utils/capitalize.ts
+++ src/utils/capitalize.ts
@@ -1,3 +1,4 @@
-export function capitalize([first, ...rest]: string): string {
-  return first.toUpperCase() + rest.join('')
+export function capitalize(string: string, all?: boolean): string {
+  const regex = new RegExp(/\b[a-z]/, all ? 'g' : '')
+  return string.toLowerCase().replace(regex, (character) => character.toUpperCase())
 }
