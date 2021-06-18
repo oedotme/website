0347a9830bb8f580eeb09bead40012efebb26072
oedotme <oedotme@gmail.com>
2021-06-18 20:01:26 +0200

    feat: add blog page

---
+++ src/pages/blog.tsx
@@ -0,0 +1,11 @@
+import { Page } from '@/layouts'
+
+export default function Blog(): JSX.Element {
+  return (
+    <Page title="Omar Elhawary's Blog">
+      <section id="projects" className="flex flex-col min-h-screen py-28">
+        <h2 className="font-semibold text-2xl">Blog</h2>
+      </section>
+    </Page>
+  )
+}
