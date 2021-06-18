4066d9b06e0f424d3420bdf9e8180240da1ad5a6
oedotme <oedotme@gmail.com>
2021-06-18 19:32:20 +0200

    feat: update header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,26 +1,11 @@
-import { Image } from '@/elements'
-import { Link } from 'wouter-preact'
+import { Link } from '@/elements'
 
 export const Header = (): JSX.Element => {
   return (
     <header>
-      <nav className="flex justify-between items-center">
-        <Link to="/">
-          <a className="" href="/">
-            <Image
-              className="h-14 w-72"
-              height="56"
-              width="288"
-              src="/assets/icons/logo.svg"
-              alt="Omar Elhawary"
-              title="Omar Elhawary"
-            />
-          </a>
-        </Link>
-
-        <button>
-          <Image className="h-6 w-6" height="24" width="24" src="/assets/icons/theme.svg" alt="theme" title="Theme" />
-        </button>
+      <nav className="flex gap-6 items-center">
+        <Link to="/">Home</Link>
+        <Link to="/blog">Blog</Link>
       </nav>
     </header>
   )
