2e5c0868f87abe87375b14092fa7da9750fe60e1
oedotme <oedotme@gmail.com>
2021-06-21 18:23:45 +0200

    feat: move blog to tsx

---
+++ src/pages/blog.tsx
@@ -0,0 +1,38 @@
+export const title = "Omar Elhawary's Blog"
+export const description = "Omar Elhawary's Blog"
+
+export default function Blog(): JSX.Element {
+  return (
+    <>
+      <h1 className="font-extrabold leading-loose mb-10 text-4xl">All Posts</h1>
+
+      <h3 className="font-semibold leading-loose my-5 text-xl max-w-6xl">Heading</h3>
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et cum eligendi asperiores molestiae cumque nihil aut
+        hic! A dolorum voluptatem, eos error, quae voluptates facere qui, natus rem quibusdam molestiae!
+      </p>
+
+      <h3 className="font-semibold leading-loose my-5 text-xl max-w-6xl">Heading</h3>
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et cum eligendi asperiores molestiae cumque nihil aut
+        hic! A dolorum voluptatem, eos error, quae voluptates facere qui, natus rem quibusdam molestiae!
+      </p>
+
+      <h3 className="font-semibold leading-loose my-5 text-xl max-w-6xl">Heading</h3>
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et cum eligendi asperiores molestiae cumque nihil aut
+        hic! A dolorum voluptatem, eos error, quae voluptates facere qui, natus rem quibusdam molestiae!
+      </p>
+
+      <h3 className="font-semibold leading-loose my-5 text-xl max-w-6xl">Heading</h3>
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et cum eligendi asperiores molestiae cumque nihil aut
+        hic! A dolorum voluptatem, eos error, quae voluptates facere qui, natus rem quibusdam molestiae!
+      </p>
+    </>
+  )
+}
