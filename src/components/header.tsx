96b0d72321a150643b4e8d5f65a0a76e41783d47
oedotme <oedotme@gmail.com>
2021-06-21 18:21:01 +0200

    feat: update header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,32 @@
+import { useLocation } from 'wouter-preact'
+
 import { Link } from '@/elements'
+import { classNames } from '@/utils'
+
+const links = [
+  { content: 'Home', to: '/' },
+  { content: 'Projects', to: '/projects' },
+  { content: 'Blog', to: '/blog' },
+]
 
 export const Header = (): JSX.Element => {
+  const [location] = useLocation()
+
   return (
     <header>
-      <nav className="flex gap-6 items-center">
-        <Link to="/">Home</Link>
-        <Link to="/blog">Blog</Link>
+      <nav>
+        <ul className="flex flex-wrap gap-6 items-center font-medium text-base">
+          {links.map(({ content, to }) => (
+            <li key={content}>
+              <Link
+                to={to}
+                className={classNames(location === to ? 'opacity-100' : 'opacity-50', 'transition-opacity delay-75')}
+              >
+                {content}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </nav>
     </header>
   )
