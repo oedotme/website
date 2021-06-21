38d25a8d9dd61a7a6a32db7990f0ce8967d57d12
oedotme <oedotme@gmail.com>
2021-06-21 18:23:02 +0200

    feat: update footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,19 +1,19 @@
 import { Link } from '@/elements'
 
 const links = [
-  { content: 'Blog', to: 'https://omarelhawary.me/blog' },
+  { content: 'Email', to: 'mailto:oedotme@gmail.com' },
+  { content: 'Resume', to: 'https://omarelhawary.me/files/omar-elhawary-resume.pdf' },
   { content: 'GitHub', to: 'https://github.com/oedotme' },
   { content: 'Twitter', to: 'https://twitter.com/oedotme' },
   { content: 'LinkedIn', to: 'https://linkedin.com/in/oedotme' },
-  { content: 'Resume', to: 'https://omarelhawary.me/files/omar-elhawary-resume.pdf' },
 ]
 
 export const Footer = (): JSX.Element => {
   return (
-    <footer className="flex flex-col-reverse items-center gap-16 font-medium md:flex-row md:gap-28">
+    <footer className="flex flex-col-reverse items-center gap-16 text-sm md:flex-row md:gap-32">
       <span className="opacity-25">&copy; 2021 Omar Elhawary</span>
 
-      <ul className="flex gap-9 flex-wrap justify-center">
+      <ul className="flex flex-wrap gap-10 justify-center">
         {links.map(({ content, to }) => (
           <li key={content}>
             <Link to={to}>{content}</Link>
