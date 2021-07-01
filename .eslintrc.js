fce02a8bd4632c07e02b518a854bb81895111e9d
oedotme <oedotme@gmail.com>
2021-07-01 10:03:44 +0200

    feat: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,5 @@ module.exports = {
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
   },
-  ignorePatterns: ['prerender.ts', '*.config.*', '*.d.ts'],
+  ignorePatterns: ['prerender.ts', '*.config.*', '.*rc.js', '*.d.ts'],
 }
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'src/**/*.{ts,tsx}': [() => 'tsc -p tsconfig.json', 'eslint -c eslint.config.js --fix'],
-  '*.{js,jsx}': 'eslint -c eslint.config.js --fix',
+  'src/**/*.{ts,tsx}': [() => 'tsc -p tsconfig.json', 'eslint --fix'],
+  '*.{js,jsx}': 'eslint --fix',
   '*.{css,html,json,md,mdx}': 'prettier -w',
 }
