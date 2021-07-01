ec9cb2e169502c55e00748cad91ef185229ab591
oedotme <oedotme@gmail.com>
2021-07-01 11:11:25 +0200

    feat: pass children to heading element

--- src/elements/heading.tsx
+++ src/elements/heading.tsx
@@ -1,10 +1,10 @@
 type Props = JSX.HTMLAttributes<HTMLHeadingElement> & { as: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' }
 
-const Heading = ({ as, ...props }: Props): JSX.Element => {
+const Heading = ({ as, children, ...props }: Props): JSX.Element => {
   const H = as
 
   const className = {
-    h1: 'font-extrabold mt-12 mb-5 text-4xl',
+    h1: 'font-extrabold mb-5 text-4xl',
     h2: 'font-bold mt-12 mb-5 text-2xl max-w-6xl',
     h3: 'font-semibold mt-10 mb-4 text-xl max-w-6xl',
     h4: 'font-semibold mt-10 mb-4 text-lg max-w-6xl',
@@ -14,14 +14,14 @@ const Heading = ({ as, ...props }: Props): JSX.Element => {
 
   return (
     <H className={className[as]} {...props}>
-      Heading
+      {children}
     </H>
   )
 }
 
-export const H1 = (): JSX.Element => <Heading as="h1" />
-export const H2 = (): JSX.Element => <Heading as="h2" />
-export const H3 = (): JSX.Element => <Heading as="h3" />
-export const H4 = (): JSX.Element => <Heading as="h4" />
-export const H5 = (): JSX.Element => <Heading as="h5" />
-export const H6 = (): JSX.Element => <Heading as="h6" />
+export const H1 = (props: Props): JSX.Element => <Heading {...props} as="h1" />
+export const H2 = (props: Props): JSX.Element => <Heading {...props} as="h2" />
+export const H3 = (props: Props): JSX.Element => <Heading {...props} as="h3" />
+export const H4 = (props: Props): JSX.Element => <Heading {...props} as="h4" />
+export const H5 = (props: Props): JSX.Element => <Heading {...props} as="h5" />
+export const H6 = (props: Props): JSX.Element => <Heading {...props} as="h6" />
