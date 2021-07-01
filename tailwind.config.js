89505944fa2cba22e11fb99d06ba62ec3484cae3
oedotme <oedotme@gmail.com>
2021-07-01 10:47:36 +0200

    feat: update tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        default: '#111111',
-        primary: '#055ad4',
-        surface: '#fdfdfd',
+        default: '#19253d',
+        primary: '#2a6cea',
+        neutral: '#f6f9ff',
+        surface: '#fcfcfc',
       },
       fontFamily: {
         sans: ['Inter', ...fontFamily.sans],
