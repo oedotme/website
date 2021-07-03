be3938be341cfbe9773e437e422f97b47ec5ccd5
oedotme <oedotme@gmail.com>
2021-07-03 11:00:31 +0200

    feat: update theme document selector

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,10 +1,10 @@
 import { storage } from '@/utils'
 
-const html = typeof window !== 'undefined' ? document.firstElementChild : undefined
+const doc = typeof window !== 'undefined' ? window.document.documentElement : undefined
 const key = 'color-scheme'
 
 export const theme = {
-  set: (value: string): void => (html?.setAttribute(key, value), storage.set(key, value)),
-  get: (): string | null | undefined => html?.getAttribute(key),
-  load: (): void => html?.setAttribute(key, storage.get(key) || 'auto'),
+  set: (value: string): void => (doc?.setAttribute(key, value), storage.set(key, value)),
+  get: (): string | null | undefined => doc?.getAttribute(key),
+  load: (): void => doc?.setAttribute(key, storage.get(key) || 'auto'),
 }
