62aea1221faf7bd83018611abacadb1f9640053f
oedotme <oedotme@gmail.com>
2021-07-06 17:00:02 +0200

    feat: update container component

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -6,9 +6,9 @@ type Props = {
 
 export const Container = ({ children }: Props): JSX.Element => {
   return (
-    <section className="container flex flex-col justify-between mx-auto max-w-5xl min-h-screen py-10 px-6 lg:px-16">
+    <section className="container flex flex-col mx-auto max-w-5xl min-h-screen py-10 px-6 lg:px-16">
       <Header />
-      <main className="min-h-xs py-14 px-2 sm:py-20">{children}</main>
+      <main className="flex-1 min-h-xs py-14 px-2 sm:py-20">{children}</main>
       <Footer />
     </section>
   )
