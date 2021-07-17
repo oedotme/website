a7404faf929746b29a544c94908ce27b60206830
oedotme <oedotme@gmail.com>
2021-07-17 14:43:17 +0200

    feat: link to blog page with tag filter

--- src/components/tags.tsx
+++ src/components/tags.tsx
@@ -1,9 +1,16 @@
+import { Link } from '@/elements'
+
 export const Tags = ({ tags }: { tags: string[] }): JSX.Element => {
   return (
-    <ul className="flex gap-3 mt-2">
+    <ul className="flex gap-3 mt-4">
       {tags.map((tag) => (
-        <li key={tag} className="bg-neutral font-medium text-default py-1 px-2 rounded-sm text-sm">
-          {tag}
+        <li key={tag}>
+          <Link
+            className="bg-elevate border-2 border-elevate font-medium text-sm py-1 px-2 rounded-sm"
+            href={`/blog?tags=${tag}`}
+          >
+            {tag}
+          </Link>
         </li>
       ))}
     </ul>
