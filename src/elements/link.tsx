ec06fef21f6a8446627cc145ce69dc851b145396
oedotme <oedotme@gmail.com>
2021-07-17 14:34:11 +0200

    feat: remove internal links default styling

--- src/elements/link.tsx
+++ src/elements/link.tsx
@@ -5,36 +5,25 @@ import { classNames } from '@/utils'
 type Props = JSX.HTMLAttributes<HTMLAnchorElement> & { href: string }
 
 const styles = {
-  base: 'outline-none',
+  base: 'outline-none text-primary underline',
   states: {
-    focus: 'focus-visible:ring-1 ring-offset-2 ring-offset-surface',
-    hover: 'hover:opacity-100',
-  },
-  type: {
-    internal: 'ring-default',
-    external: 'text-primary underline ring-primary',
+    focus: 'ring-primary ring-offset-2 ring-offset-surface focus-visible:ring-1',
   },
 }
 
 export const Link = ({ children, className, href, ...props }: Props): JSX.Element => {
-  const { base, states, type } = styles
+  const { base, states } = styles
 
   if (href?.startsWith('/')) {
     return (
-      <InternalLink className={classNames(base, states.focus, states.hover, type.internal, className)} to={href}>
+      <InternalLink className={className} to={href}>
         {children}
       </InternalLink>
     )
   }
 
   return (
-    <a
-      {...props}
-      className={classNames(base, states.focus, states.hover, type.external)}
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
+    <a {...props} className={classNames(base, states.focus)} href={href} target="_blank" rel="noopener noreferrer">
       {children}
     </a>
   )
