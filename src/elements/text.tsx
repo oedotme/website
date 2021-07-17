dd26ad7b95dca3fed583a9a7d801884051835d7c
oedotme <oedotme@gmail.com>
2021-07-17 14:42:12 +0200

    feat: update text line height

--- src/elements/text.tsx
+++ src/elements/text.tsx
@@ -2,7 +2,7 @@ type Props = JSX.HTMLAttributes<HTMLParagraphElement>
 
 export const Text = ({ children, ...props }: Props): JSX.Element => {
   return (
-    <p className="leading-relaxed mb-4 text-base" {...props}>
+    <p className="leading-loose mb-4 text-base" {...props}>
       {children}
     </p>
   )
