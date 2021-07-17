95da3698812ac62c8abc31f3a9369afa47922c6b
oedotme <oedotme@gmail.com>
2021-07-17 14:26:42 +0200

    feat: remove active header link highlight

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,6 @@
-import { useLocation } from 'wouter-preact'
-
 import { Icons } from '@/config'
 import { Link } from '@/elements'
-import { classNames, theme } from '@/utils'
+import { theme } from '@/utils'
 
 const links = [
   { content: 'Home', href: '/' },
@@ -11,8 +9,6 @@ const links = [
 ]
 
 export const Header = (): JSX.Element => {
-  const [location] = useLocation()
-
   const toggleTheme = () => theme.set(['auto', 'light'].includes(theme.get() || '') ? 'dark' : 'light')
 
   return (
@@ -20,15 +16,7 @@ export const Header = (): JSX.Element => {
       <nav className="flex justify-between">
         <ul className="flex flex-wrap gap-2 items-center font-medium text-base sm:gap-6">
           {links.map(({ content, href }) => (
-            <li
-              key={content}
-              className={classNames(
-                'transition-opacity delay-75',
-                (location === href && href.length === 1) || (location.startsWith(href) && href.length > 1)
-                  ? 'opacity-100'
-                  : 'opacity-50'
-              )}
-            >
+            <li key={content}>
               <Link href={href} className="p-2">
                 {content}
               </Link>
