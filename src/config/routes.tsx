fedb4493eaa191971becd0040178016375978dbe
oedotme <oedotme@gmail.com>
2021-07-18 17:36:57 +0200

    feat: add posts titles and tags to routes config

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -6,6 +6,14 @@ const files = import.meta.globEager('../pages/**/*.{md,mdx,tsx}')
 
 const paths = { '/404': '' }
 
+type Post = { path: string; title: string; tags: string[]; date: string }
+
+type Props = {
+  posts?: Post[]
+  titles?: Record<string, number[]>
+  tags?: string[]
+}
+
 const routes = Object.keys(files)
   .reverse()
   .map((file) => {
@@ -14,13 +22,24 @@ const routes = Object.keys(files)
 
     return {
       path: path in paths ? paths[path as keyof typeof paths] : path,
-      component: files[file]?.default as () => JSX.Element,
+      component: files[file]?.default as (props: Props) => JSX.Element,
       title: files[file]?.title as string,
       description: files[file]?.description as string,
       tags: files[file]?.tags as string[],
       date: files[file]?.date as string,
     }
   })
+
+const posts = routes
+  .filter((file) => file.path.includes('/blog/'))
+  .sort((a, b) => +new Date(b.date) - +new Date(a.date))
+
+const titles: Record<string, number[]> = posts.reduce(
+  (titles, current, index) => ({ ...titles, [current.title]: [index + 1, index, index - 1] }),
+  {}
+)
+
+const filters = [...new Set(posts.flatMap((post) => post.tags))].sort()
 
 export function Routes(): JSX.Element {
   return (
@@ -30,8 +49,14 @@ export function Routes(): JSX.Element {
           {(params) => (
             <>
               {path !== paths['/404'] && <Head title={title} description={description} date={date}></Head>}
-              {path.startsWith('/blog/') && <Post.Header {...{ title, date, tags }} />}
-              <Component {...params} />
+
+              {path.startsWith('/blog/') ? (
+                <Post {...{ posts, titles, path, title, date, tags }}>
+                  <Component {...params} />
+                </Post>
+              ) : (
+                <Component {...{ ...params, posts, titles, tags: filters }} />
+              )}
             </>
           )}
         </Route>
