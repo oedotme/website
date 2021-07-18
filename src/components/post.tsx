941c6b3f1a4d2c666dad88e8223bf46bbcf046e1
oedotme <oedotme@gmail.com>
2021-07-18 17:42:00 +0200

    feat: add previous and next post buttons

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,32 +1,51 @@
 import { Tags } from '@/components'
+import { env } from '@/config'
 import { H1, Link } from '@/elements'
 
-type Props = { children: Children }
+type Post = { path: string; title: string; tags: string[]; date: string }
 
-export const Post = ({ children }: Props): JSX.Element => {
-  return <>{children}</>
+type Props = {
+  children: Children
+  posts: Post[]
+  titles: Record<string, number[]>
 }
 
-const PostHeader = ({ title, tags, date }: { title: string; tags: string[]; date: string }): JSX.Element => {
+export const Post = ({ children, posts, titles, title, tags, date }: Props & Post): JSX.Element => {
+  const [prev = -1, current = 0, next = -1] = titles?.[title]
+
   return (
-    <section>
-      <Link className="inline-block mb-6 -mx-4 py-2 px-4" href="/blog">
-        ⟵
-      </Link>
-      <H1>{title}</H1>
-      <span className="text-sm">{date}</span>
-      <Tags tags={tags} />
-    </section>
+    <>
+      <section className="mb-16">
+        <H1>{title}</H1>
+        <span className="text-sm">{date}</span>
+        <Tags tags={tags} />
+      </section>
+
+      {children}
+
+      <section className="my-8 text-sm">
+        <div className="flex gap-4 text-sm my-4">
+          {prev <= posts.length - 1 && (
+            <Link
+              className="font-medium rounded-sm truncate py-2 w-40"
+              href={posts[prev].path}
+              title={posts[prev].title}
+            >
+              &larr;&ensp; Previous post
+            </Link>
+          )}
+
+          {next >= 0 && posts.length > 0 && (
+            <Link
+              className="font-medium rounded-sm truncate py-2 w-40 text-right ml-auto"
+              href={posts[next].path}
+              title={posts[next].title}
+            >
+              Next post &ensp;&rarr;
+            </Link>
+          )}
+        </div>
+      </section>
+    </>
   )
 }
-
-const PostFooter = (): JSX.Element => {
-  return (
-    <section>
-      <span>Share</span>
-    </section>
-  )
-}
-
-Post.Header = PostHeader
-Post.Footer = PostFooter
