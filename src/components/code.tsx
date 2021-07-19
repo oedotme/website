ead185f22be2816a32a5b6f9e08f53e8e28d268f
oedotme <oedotme@gmail.com>
2021-07-19 18:07:20 +0200

    feat: add code copy button

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -1,3 +1,5 @@
+import { useRef, useState } from 'preact/hooks'
+
 export const Code = ({ children, ...props }: JSX.HTMLAttributes<HTMLElement>): JSX.Element => {
   return (
     <code {...props} className="bg-neutral border border-elevate font-medium text-sm rounded py-1 px-2">
@@ -6,18 +8,45 @@ export const Code = ({ children, ...props }: JSX.HTMLAttributes<HTMLElement>): J
   )
 }
 
-type Props = JSX.HTMLAttributes<HTMLPreElement> & { children: { props: JSX.HTMLAttributes<HTMLElement> } }
+export const Pre = ({ children, ...props }: JSX.HTMLAttributes<HTMLPreElement>): JSX.Element => {
+  const ref = useRef<HTMLPreElement>(null)
+  const [copy, setCopy] = useState('Copy')
 
-export const Pre = ({ children, ...props }: Props): JSX.Element => {
-  const { filename = '' } = children?.props
+  const handleClick = () => {
+    try {
+      const code = ref.current?.firstChild
+      const range = document.createRange()
+      const selection = window.getSelection()
+
+      range.selectNodeContents(code as Node)
+      selection?.removeAllRanges()
+      selection?.addRange(range)
+      document.execCommand('copy')
+      selection?.removeAllRanges()
+
+      setCopy('Copied!')
+    } catch {
+      setCopy('Not supported')
+    }
+  }
 
   return (
-    <pre
-      {...props}
-      className="bg-neutral border border-elevate my-7 leading-normal overflow-x-scroll rounded text-sm p-5"
-    >
-      {filename && <span className="block font-medium mb-5 text-comment text-xs">{filename}</span>}
-      {children}
-    </pre>
+    <div className="relative group">
+      <button
+        className="absolute bg-elevate font-medium hidden top-2 right-2 text-xs py-1 px-2 rounded-sm group-hover:block"
+        onClick={handleClick}
+        onBlur={() => setCopy('Copy')}
+      >
+        {copy}
+      </button>
+
+      <pre
+        {...props}
+        className="bg-neutral border border-elevate my-7 leading-normal overflow-x-scroll rounded text-sm p-5 pr-6"
+        ref={ref}
+      >
+        {children}
+      </pre>
+    </div>
   )
 }
