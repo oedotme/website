0b93eea1cc9d6d9d7f56d3536fe862b366e14f59
oedotme <oedotme@gmail.com>
2021-07-25 21:14:18 +0200

    feat: update lab page

---
+++ src/pages/lab.tsx
@@ -0,0 +1,26 @@
+import { Head } from '@/components'
+import { H1, H2, Link, List } from '@/elements'
+
+const content = [
+  ['Outline', 'Video/Chat web application', 'https://outlineapp.netlify.app'],
+  ['Render', 'Opinionated React template', 'https://github.com/oedotme/render'],
+]
+
+export default function Lab() {
+  return (
+    <>
+      <Head title="Lab" description="Omar Elhawary's software development lab" />
+
+      <H1>Lab</H1>
+
+      <H2>Links</H2>
+      <List>
+        {content.map(([name, description, link]) => (
+          <List.Item key={name}>
+            <Link href={link}>{name}</Link> - {description}
+          </List.Item>
+        ))}
+      </List>
+    </>
+  )
+}
