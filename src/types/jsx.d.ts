0d15640eb5dac910ccddae08e547074a9041f0ec
oedotme <oedotme@gmail.com>
2021-07-25 19:23:49 +0200

    feat: add jsx and prism to declaration types

---
+++ src/types/jsx.d.ts
@@ -0,0 +1,8 @@
+type Children = React.ReactNode
+
+type HTMLAnchorProps = React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>
+type HTMLCodeProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
+type HTMLHeadingProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>
+type HTMLLIProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLLIElement>, HTMLLIElement>
+type HTMLParagraphProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement>
+type HTMLUListProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLUListElement>, HTMLUListElement>
