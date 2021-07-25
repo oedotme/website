25e6858bef0bea49af878447cada729da408f78e
oedotme <oedotme@gmail.com>
2021-07-25 21:13:05 +0200

    feat: add custom _app

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import '@/styles/theme.css'
+import '@/styles/global.css'
+import '@/styles/syntax.css'
+import '@/styles/layers.css'
+
+import { Container } from '@/components'
+import { theme } from '@/utils'
+
+theme.load()
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <Component {...pageProps} />
+    </Container>
+  )
+}
