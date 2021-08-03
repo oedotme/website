4c6a025839c482c1a8651c4cdd4fce4b98d9d44b
oedotme <oedotme@gmail.com>
2021-08-03 07:13:13 +0200

    feat: update container max width

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -4,7 +4,7 @@ type Props = { children: Children }
 
 export const Container = ({ children }: Props) => {
   return (
-    <section className="container flex flex-col mx-auto max-w-5xl min-h-screen py-10 px-3 lg:px-16">
+    <section className="container flex flex-col mx-auto max-w-4xl min-h-screen py-10 px-3 lg:px-16">
       <Header />
       <main className="flex-1 min-h-xs py-14 px-2 sm:py-20">{children}</main>
       <Footer />
