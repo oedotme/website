1341ee177eafb129b4e0f727a4fc2340ac4badcb
oedotme <oedotme@gmail.com>
2021-08-06 15:56:28 +0200

    feat: update link element

--- src/elements/link.tsx
+++ src/elements/link.tsx
@@ -6,27 +6,27 @@ type Props = HTMLAnchorProps & { as?: string }
 
 const styles = {
   base: 'border-b border-elevate outline-none text-primary',
-  states: {
-    focus: 'ring-primary ring-offset-2 ring-offset-surface focus-visible:ring-1',
-  },
+  focus: 'ring-primary ring-offset-2 ring-offset-surface focus-visible:ring-1',
 }
 
-export const Link = ({ children, className, href, as, ...props }: Props) => {
-  const { base, states } = styles
+export const Link = ({ className, href = '/', as, ...props }: Props) => {
+  const internal = href?.startsWith('/')
 
-  if (href?.startsWith('/')) {
+  if (internal) {
     return (
       <InternalLink href={href} as={as}>
-        <a className={className} href={href}>
-          {children}
-        </a>
+        <a className={className} href={href} {...props} />
       </InternalLink>
     )
   }
 
   return (
-    <a {...props} className={classNames(base, states.focus)} href={href} target="_blank" rel="noopener noreferrer">
-      {children}
-    </a>
+    <a
+      {...props}
+      className={classNames(styles.base, styles.focus)}
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+    />
   )
 }
