76deb35bd77dcfb841598108b49c92cce765695f
oedotme <oedotme@gmail.com>
2021-08-06 22:18:24 +0200

    feat: set code text sm only inside p

--- src/elements/code.tsx
+++ src/elements/code.tsx
@@ -26,7 +26,7 @@ export const Code = ({ children, ...props }: Props) => {
 
   if (!props.className?.includes('language-')) {
     return (
-      <code {...props} className="bg-neutral border border-elevate font-bold text-sm rounded py-1 px-2">
+      <code {...props} className="bg-neutral border border-elevate font-bold rounded py-1 px-2">
         {children}
       </code>
     )
