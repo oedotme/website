2cdf4d8f5bff1489d018d6e40fb79352bb134dd4
oedotme <oedotme@gmail.com>
2021-08-14 18:36:06 +0200

    feat: add sitemap script

---
+++ sitemap.js
@@ -0,0 +1,34 @@
+import fs from 'fs/promises'
+import prettier from 'prettier'
+
+const getFiles = (path) => fs.readdir(process.cwd() + path)
+
+const generateSitemap = async () => {
+  const valid = /^[a-z].+\.tsx$/
+  const pages = (await getFiles('/src/pages')).map((page) => (valid.test(page) ? `/${page.replace('.tsx', '')}` : ''))
+  const posts = (await getFiles('/content/posts')).map((post) => `/blog/${post.replace('.mdx', '')}`)
+
+  const urls = [...pages, ...posts].filter(Boolean)
+
+  const sitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
+      ${urls
+        .map(
+          (url) => `
+            <url>
+              <loc>${'https://omarelhawary.me' + `${url === '/index' ? '' : url}`}</loc>
+            </url>
+          `
+        )
+        .join('')}
+    </urlset>
+  `
+
+  const config = await prettier.resolveConfig('./prettier.config.js')
+  const formatted = prettier.format(sitemap, { ...config, parser: 'html' })
+
+  await fs.writeFile(process.cwd() + '/public/sitemap.xml', formatted)
+}
+
+generateSitemap()
