01d5caa79109f45f432a844fb7ddf1a1f6687bcd
oedotme <oedotme@gmail.com>
2021-08-15 13:22:51 +0200

    feat: allow overriding link class name

--- src/elements/link.tsx
+++ src/elements/link.tsx
@@ -9,24 +9,24 @@ const styles = {
   focus: 'ring-primary ring-offset-2 ring-offset-surface focus-visible:ring-1',
 }
 
-export const Link = ({ className, href = '/', as, ...props }: Props) => {
+export const Link = ({ href = '/', as, ...props }: Props) => {
   const internal = href?.startsWith('/')
 
   if (internal) {
     return (
       <InternalLink href={href} as={as}>
-        <a className={className} href={href} {...props} />
+        <a className={props.className} href={href} {...props} />
       </InternalLink>
     )
   }
 
   return (
     <a
-      {...props}
       className={classNames(styles.base, styles.focus)}
       href={href}
       target="_blank"
       rel="noopener noreferrer"
+      {...props}
     />
   )
 }
