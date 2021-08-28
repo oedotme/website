73f58e0866ec1974d160e0c2cad881432de76864
oedotme <oedotme@gmail.com>
2021-08-28 10:37:23 +0200

    feat: improve theme detection for first toggle

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,10 +1,13 @@
 import { storage } from '@/utils'
 
 const doc = typeof window !== 'undefined' ? window.document.documentElement : undefined
+const dmq = typeof window !== 'undefined' ? window.matchMedia('(prefers-color-scheme: dark)').matches : false
 const key = 'color-scheme'
 
+type Theme = 'auto' | 'dark' | 'light'
+
 export const theme = {
-  set: (value: string): void => (doc?.setAttribute(key, value), storage.set(key, value)),
-  get: (): string => doc?.getAttribute(key) || '',
+  set: (value: Theme): void => (doc?.setAttribute(key, value), storage.set(key, value)),
+  get: (): Theme => (doc?.getAttribute(key) === 'auto' ? (dmq ? 'dark' : 'light') : (doc?.getAttribute(key) as Theme)),
   load: (): void => doc?.setAttribute(key, storage.get(key) || 'auto'),
 }
