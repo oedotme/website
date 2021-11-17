2e663c2dd6dcb69910b6f53782ffa7df298b5479
oedotme <oedotme@gmail.com>
2021-11-17 06:30:16 +0200

    feat: update preact usage to production client builds

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-const withPreact = require('next-plugin-preact')
-
-module.exports = withPreact({
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-})
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.resolve.alias['react'] = 'preact/compat'
+      config.resolve.alias['react-dom'] = 'preact/compat'
+    }
+    return config
+  },
+}
