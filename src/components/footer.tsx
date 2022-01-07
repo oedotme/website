0b4e6763487620f1e4bc166bec3d2cceab25b4ba
oedotme <oedotme@gmail.com>
2022-01-07 22:43:32 +0200

    chore: update copyright

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ import { Link } from '@/elements'
 export const Footer = () => {
   return (
     <footer className="flex flex-col-reverse items-center justify-between gap-8 p-2 text-sm md:flex-row md:gap-32">
-      <span className="text-default opacity-70">&copy; 2021 Omar Elhawary</span>
+      <span className="text-default opacity-70">&copy; 2022 Omar Elhawary</span>
 
       <nav>
         <ul className="flex flex-wrap gap-4 justify-center sm:gap-8">
