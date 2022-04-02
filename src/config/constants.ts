9dea42a6688ad28daf10a0c0f8b1a7f7e9f77e97
oedotme <oedotme@gmail.com>
2022-04-02 11:13:35 +0200

    feat: add new environment and cli items to uses

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -59,18 +59,20 @@ export const constants = {
   uses: {
     environment: [
       { name: 'Arch', description: 'Lightweight Linux distribution', link: 'https://archlinux.org' },
-      { name: 'Brave', description: 'Privacy focused browser', link: 'https://brave.com' },
+      { name: 'Brave', description: 'Privacy focused web browser', link: 'https://brave.com' },
       { name: 'Fish', description: 'User friendly interactive shell', link: 'https://fishshell.com' },
       { name: 'Gnome', description: 'Desktop environment - Wayland', link: 'https://www.gnome.org' },
       { name: 'JetBrains', description: 'Font with ligatures support', link: 'https://www.jetbrains.com/lp/mono' },
       { name: 'Kitty', description: 'GPU accelerated terminal emulator', link: 'https://sw.kovidgoyal.net/kitty' },
       { name: 'Neovim', description: 'Vim-based text editor', link: 'https://neovim.io' },
+      { name: 'Raycast', description: 'Better Spotlight - Productivity', link: 'https://www.raycast.com' },
       { name: 'Tmux', description: 'Terminal sessions management', link: 'https://github.com/tmux/tmux' },
     ],
     cli: [
       { name: 'FZF', description: 'General purpose fuzzy finder', link: 'https://github.com/junegunn/fzf' },
       { name: 'PNPM', description: 'JavaScript package manager', link: 'https://pnpm.io' },
       { name: 'Ripgrep', description: 'Better grep', link: 'https://github.com/BurntSushi/ripgrep' },
+      { name: 'Slides', description: 'Terminal-based presentation', link: 'https://github.com/maaslalani/slides' },
       { name: 'Stow', description: 'Dotfiles management', link: 'https://www.gnu.org/software/stow' },
       { name: 'Vifm', description: 'Vim-based file manager', link: 'https://vifm.info' },
       { name: 'Volta', description: 'JavaScript tool manager', link: 'https://volta.sh' },
