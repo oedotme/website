3a478d106171a606fba15a68218feb90e2a420fc
oedotme <oedotme@gmail.com>
2022-07-08 11:04:34 +0200

    feat: re-enable preact webpack aliasing

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  // webpack: (config, { dev, isServer }) => {
-  //   if (!dev && !isServer) {
-  //     config.resolve.alias['react'] = 'preact/compat'
-  //     config.resolve.alias['react-dom'] = 'preact/compat'
-  //   }
-  //   return config
-  // },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.resolve.alias['react'] = 'preact/compat'
+      config.resolve.alias['react-dom'] = 'preact/compat'
+    }
+    return config
+  },
 }
