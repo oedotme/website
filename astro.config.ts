fbdba03d50b4b21afe512af6edfdd4f6f43cfbfb
oedotme <oedotme@gmail.com>
2023-01-30 20:38:19 +0200

    feat: temp plugin for rehype meta data

--- astro.config.ts
+++ astro.config.ts
@@ -5,10 +5,22 @@ import sitemap from '@astrojs/sitemap'
 import solid from '@astrojs/solid-js'
 import tailwind from '@astrojs/tailwind'
 import prism from 'rehype-prism-plus'
+import { visit } from 'unist-util-visit'
+
+const meta = () => {
+  return (tree) => {
+    visit(tree, 'element', (node) => {
+      if (node.type === 'element' && node.tagName === 'code' && node.properties?.metastring) {
+        node.data ??= {}
+        node.data.meta = node.properties.metastring
+      }
+    })
+  }
+}
 
 export default defineConfig({
   site: 'https://omarelhawary.me',
   vite: { resolve: { alias: { '@': '/src' } } },
-  markdown: { syntaxHighlight: 'prism', rehypePlugins: [prism] },
+  markdown: { syntaxHighlight: false, rehypePlugins: [meta, prism] },
   integrations: [mdx(), prefetch(), sitemap(), solid(), tailwind()],
 })
