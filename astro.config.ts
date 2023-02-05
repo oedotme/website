04e20f5ae4b0248f1ddd65d36d983a14415e3ea9
oedotme <oedotme@gmail.com>
2023-02-05 11:44:48 +0100

    chore: use latest rehype prism plugin for line highlight

--- astro.config.ts
+++ astro.config.ts
@@ -5,22 +5,10 @@ import sitemap from '@astrojs/sitemap'
 import solid from '@astrojs/solid-js'
 import tailwind from '@astrojs/tailwind'
 import prism from 'rehype-prism-plus'
-import { visit } from 'unist-util-visit'
-
-const meta = () => {
-  return (tree) => {
-    visit(tree, 'element', (node) => {
-      if (node.type === 'element' && node.tagName === 'code' && node.properties?.metastring) {
-        node.data ??= {}
-        node.data.meta = node.properties.metastring
-      }
-    })
-  }
-}
 
 export default defineConfig({
   site: 'https://omarelhawary.me',
   vite: { resolve: { alias: { '@': '/src' } } },
-  markdown: { syntaxHighlight: false, rehypePlugins: [meta, prism] },
+  markdown: { syntaxHighlight: false, rehypePlugins: [prism] },
   integrations: [mdx(), prefetch(), sitemap(), solid(), tailwind()],
 })
