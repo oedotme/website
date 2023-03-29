7a4efd3acbfa540c2500e32ab14e14f7809b83f5
oedotme <oedotme@gmail.com>
2023-03-29 07:22:39 +0200

    chore: sort tailwind classes

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -5,13 +5,13 @@ export const Code = (props: JSX.HTMLAttributes<HTMLElement>) => {
     <Show
       when={props.class?.includes('language-')}
       fallback={
-        <code {...props} class="rounded bg-element py-1 px-1">
+        <code {...props} class="rounded bg-element px-1 py-1">
           {props.children}
         </code>
       }
     >
       <div class="group relative">
-        <button class="copy-code absolute top-2 right-2 z-10 hidden rounded-sm bg-elevate py-1 px-2 text-xs font-medium group-hover:block">
+        <button class="copy-code absolute right-2 top-2 z-10 hidden rounded-sm bg-elevate px-2 py-1 text-xs font-medium group-hover:block">
           Copy
         </button>
 
